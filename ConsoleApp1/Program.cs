4ed227b155536419a771c32f5161447c63ae709e
olegnastas <olegnastas1@mail.ru>
2024-01-17 22:53:01 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class Weapon
+    {
+       abstract public void Fire();
+
+    }
+
+    class Player
+    {
+        public void Fire(Weapon weapon)
+        {
+            weapon.Fire();
+        }
+    }
+
+    class Gun : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine(" ПЫЩ ПЫЩ !!!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            player.Fire(new Gun());
+        }
+    }
+}
